f66d10eade91cf459f16ebe1b14a5cec4cf3e422
young0519 <wkdwlsdud0519@gmail.com>
2023-09-19 22:12:58 +0900

    구글 로그인 기능 구현하기

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,9 +1,28 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import * as s from "../styles/SignStyle"
 import * as i from "../styles/InputStyle";
 import * as b from "../styles/ButtonStyle";
+import { auth } from "../firebase";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useState } from "react";
 
 const Signin = () => {
+
+  const [userData, setUserData] = useState(null);
+  const navigate = useNavigate();
+
+  function handleGoogleLogin() {
+    const provider = new GoogleAuthProvider(); //provider 구글 설정
+    signInWithPopup(auth, provider) // 팝업창띄워서 로그인
+      .then((data) => {
+        setUserData(data.user); // user data 설정
+        console.log(data); // console에UserCredentialImpl 출력
+        navigate('/');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
   return (
     <s.BoxContainer className="signContainer">
@@ -19,7 +38,7 @@ const Signin = () => {
       <Link to='/signup'>회원가입</Link>
       <b.LoginBtn type="button">Login</b.LoginBtn>
       <hr></hr>
-      <b.SocialLoginBtn>Google로 로그인하기</b.SocialLoginBtn>
+      <b.SocialLoginBtn type="button" onClick={handleGoogleLogin}>Google로 로그인하기</b.SocialLoginBtn>
     </s.BoxContainer>
   )
 
